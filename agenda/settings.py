8c314e8d79c48cff6face13f87396cd595146452
Artulanez Souza <artulanez.luz@maida.health>
2023-01-29 13:34:52 -0300

    Adicionando filtros por data e criando função para retornar json

--- agenda/settings.py
+++ agenda/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-x-7sbhriorr6oa=)i#rih2gd49ttjr5()!lic1sc4gxnkzpp*g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -105,15 +105,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/sao_Paulo'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
